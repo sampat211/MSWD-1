69f5733943874399b20e6fb0747119bd8ba350bb
GayathriSowmya2607 <73687075+GayathriSowmya2607@users.noreply.github.com>
2021-06-01 16:45:54 +0530

    Add files via upload

---
+++ part1/1.2/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Part = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+      {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part />
+      <Part />
+      <Part />
+    </div>
+  )
+}
+
+export default Content;
